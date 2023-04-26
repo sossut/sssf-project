5f94918e0c8085c576624627bab8e0431c569c0d
sossut <mikki.alakari@elisanet.fi>
2023-04-26 21:34:47 +0300

    spot

--- src/api/resolvers/spotResolver.ts
+++ src/api/resolvers/spotResolver.ts
@@ -1,6 +1,8 @@
 import {PalletSpot} from '../../interfaces/PalletSpot';
 import {Spot} from '../../interfaces/Spot';
 import spotModel from '../models/spotModel';
+import gapModel from '../models/gapModel';
+import rowModel from '../models/rowModel';
 
 export default {
   PalletSpot: {
@@ -14,6 +16,22 @@ export default {
     },
     spotById: async (_parent: undefined, args: Spot) => {
       return await spotModel.findById(args.id);
+    },
+    // find spot by row and gap
+    spotByRowGap: async (_parent: undefined, args: any) => {
+      const row = await rowModel.findOne({rowNumber: args.rowNumber});
+
+      const gap = await gapModel.findOne({
+        row: row?._id,
+        gapNumber: args.gapNumber,
+      });
+
+      const spot = await spotModel.findOne({
+        spotNumber: args.spotNumber,
+        gap: gap?._id,
+      });
+
+      return spot;
     },
   },
   Mutation: {
